90390e87b0f4b9fb5347cd9aca04677f2d887183
Roksolana <sonia190593@gmail.com>
2026-09-24 13:57:29 +0300

    fix fix: correct modern-normalize import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
 import App from "./components/App/App";
 
 const queryClient = new QueryClient();
